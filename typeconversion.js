489e34017046f8c36dece1e09ff08ea3a380418f
Shaima <shaima.anvar@gmail.com>
2022-11-23 11:28:03 -0500

    testing == and ===

---
+++ typeconversion.js
@@ -0,0 +1,14 @@
+// let a=10;
+// let b='10';
+// if (a===b){
+//     console.log('a and b are equal');
+// }else{
+//     console.log('a and b are unequal');
+// }
+let a=10;
+let b='10';
+if (a==b){
+    console.log('a and b are equal');
+}else{
+    console.log('a and b are unequal');
+}
