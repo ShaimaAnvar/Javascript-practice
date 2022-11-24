96274da010bdd6a481413f839875215751bba696
Shaima <shaima.anvar@gmail.com>
2022-11-24 09:51:52 -0500

    typeof operator

---
+++ typeof.js
@@ -0,0 +1,8 @@
+let a;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof NaN);
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof function(){});
+console.log(typeof []);
--- variableDeclarations.js
+++ variableDeclarations.js
@@ -1,7 +1,14 @@
-var a=10;
-var a= 7;
-a=3;
-a=9
-console.log(a);
-let b;
-console.log(c);
+// var a=10;
+// var a= 7;
+// a=3;
+// a=9
+// console.log(a);
+// let b;
+// console.log(c);
+
+x=13;
+x='abc';
+var y;
+console.log(typeof z);
+console.log(typeof null);
+console.log(typeof NaN);
